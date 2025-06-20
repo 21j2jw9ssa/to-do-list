7444a89b5df32407e4bf916cac27ae1931bc35df
侯柏瑞 <121330378+21j2jw9ssa@users.noreply.github.com>
2025-06-20 11:57:42 +0800

    Indicate the current page

---
+++ nav.js
@@ -0,0 +1,13 @@
+document.addEventListener( "DOMContentLoaded", function() {
+
+  // Get all tabs of the navigator bar
+  const navBar = document.querySelectorAll( "#nav_of_links a" ) ;
+
+  // Then detect the tab with an empty hyperlink reference:
+  // it is the current page.
+  for ( let i = 0 ; i < navBar.length ; i++ ) {
+    if ( navBar[i].getAttribute("href").length === 0 ) {
+      navBar[i].classList.add("current_page") ;
+    } 
+  }
+}) ;
