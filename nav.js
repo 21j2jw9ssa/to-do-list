c47850f04365d4246f99f0ec9e1c5e72479a3ffd
侯柏瑞 <121330378+21j2jw9ssa@users.noreply.github.com>
2025-08-24 00:47:35 +0800

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,15 +1,21 @@
 document.addEventListener( "DOMContentLoaded", function() {
 
   // Get all tabs of the navigator bar
-  const navBar = document.querySelectorAll( "#nav-of-links a" ) ;
+  const navBar = document.getElementById("nav-of-links") ;
+  const navBarCells = navBar.getElementsByTagName( "a" ) ;
 
   // Then detect the tab with an empty hyperlink reference:
   // it is the current page.
-  for ( let i = 0 ; i < navBar.length ; i++ ) {
-    if ( navBar[i].getAttribute("href").length === 0 ) {
-      navBar[i].classList.add("current-page") ;
+  for ( let i = 0 ; i < navBarCells.length ; i++ ) {
+    if ( navBarCells[i].getAttribute("href").length === 0 ) {
+      navBarCells[i].classList.add("current-page") ;
     } 
   }
 
+  window.addEventListener( "scroll", () => {
+    if ( window.scrollY > 0 )
+      navBar.classList.add("is-stuck") ;
+    else
+      navBar.classList.remove("is-stuck") ;
+  }) ;
 }) ;
-
